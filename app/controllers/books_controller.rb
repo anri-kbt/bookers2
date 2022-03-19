e74923a7994837f9d643d87ab690152645e0624b
EC2 Default User <ec2-user@ip-172-31-36-47.ec2.internal>
2022-03-19 08:28:58 +0000

    {add}bookers2-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
   def index
     @books=Book.all
+    @book=Book.new
     @user=current_user
   end
 
   def create
-    @book=Book.new(book_params)
-    @book.user_id=current_user.id
+    @book=Book.new(book_params) #フォームから送られてきたデータをストロングパラメータを経由して@bookに代入
+    @book.user_id=current_user.id #ユーザーIDの情報をdeviseメソッドを使ってログインしている自分のIDを
     if @book.save
       redirect_to book_path(@book.id)
     else
@@ -17,12 +18,28 @@ class BooksController < ApplicationController
   end
 
   def show
-    @books=Book.find(params[:id])
-    @book_new=Book.new
+    @books=@user.book
+    @book=Book.new
+    @user=User.find(params[:id])
   end
 
   def edit
     @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
